85a0acc367bbf1af30163dce644bce3ce609e232
erickhansx <erickhansx@gmail.com>
2023-07-02 12:26:16 -0600

    Add props to StarRating Component and proptypes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.jsx'
 // import './index.css'
 
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Amazing']}
+    />
+    <StarRating
+      maxRating={5}
+      size={34}
+      color="red"
+      className="test"
+      defaultRating={1}
+    />
+    <Test />
   </React.StrictMode>,
 );
